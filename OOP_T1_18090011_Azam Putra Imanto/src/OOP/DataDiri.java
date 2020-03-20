3d0f6846f94bd551ddace4e576092e05ec4f3fe7
AzamPutra22 <56184970+AzamPutra22@users.noreply.github.com>
2020-03-20 17:34:08 +0700

    Add files via upload

---
+++ OOP_T1_18090011_Azam Putra Imanto/src/OOP/DataDiri.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP;
+
+/**
+ *
+ * @author AZAM
+ */
+public class DataDiri {
+    public static void main(String[]args){
+        Mahasiswa ti = new Mahasiswa();
+                
+                ti.NIM ="18090011\n";
+                ti.Nama = "Azam Putra Imanto\n";
+                ti.Kelas ="4D\n";
+                
+                ti.Hasil();
+                
+    }
+    
+}
---
+++ OOP_T1_18090011_Azam Putra Imanto/src/OOP/Mahasiswa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP;
+
+/**
+ *
+ * @author AZAM
+ */
+public class Mahasiswa {
+    String NIM;
+    String Nama;
+    String Kelas;
+    
+    public void Hasil(){
+        System.out.print(NIM);
+        System.out.print(Nama);
+        System.out.print(Kelas);
+    }
+}
